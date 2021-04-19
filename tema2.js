7ffaa615974bfcc9badaaff1dc0a94339e215055
shaylcm <78311101+shaylcm@users.noreply.github.com>
2021-04-19 12:46:52 -0300

    Add files via upload

---
+++ tema2.js
@@ -0,0 +1,58 @@
+
+//variaveis usadas
+var primeiro = document.querySelector("#primeiro");
+var nav = document.querySelector("nav");
+var body = document.querySelector("body");
+var rodape= document.querySelector("#rodape");
+var botao= document.querySelector(".botao");
+var boto= document.querySelector(".boto");
+var bot=document.querySelector("#bot");
+var apaga =document.querySelector("#apaga");
+
+
+
+//alerta 1
+alert("  --------------------BEM VINDOS!------------------------ );
+
+//MUDAR TEMA DA PÁGINA
+bot.onclick = function(){
+	alert("ATENÇÃO:Ao clicar na logo 'MUSICA' o tema da página será alterado e retornará ao tema original somente quando a página for atualizada ,OU,quando clicares em um dos títulos no cabeçalho antes do menu nav;:");
+	primeiro.style.backgroundColor = "#0d3b66";
+	nav.style.backgroundColor=" #f0544f";
+	document.body.style.backgroundImage = "url('musica2.png')";
+    rodape.style.backgroundColor="#0d3b66";
+    botao.style.backgroundColor="#0d3b66";
+
+
+
+};
+//VOTRA PARA O TEMA ORIGINAL DA
+boto.onclick=function(){
+	primeiro.style.backgroundColor = "#008B8B";
+	nav.style.backgroundColor=" black";
+	document.body.style.backgroundImage = "url('music3.png')";
+    rodape.style.backgroundColor="#008B8B";
+    botao.style.backgroundColor="#008B8B";
+    select.style.backgroundColor="#008B8B";
+
+
+};
+
+	botao.onclick = function(){
+		if(document.formu.nome.value!="" &&  document.formu.email.value !=""  &&   document.formu.texto.value!=""){
+			alert("ENVIADO COM SUCESSO!");
+			
+
+	}
+		else {
+			alert("opss! algum campo  vazio");
+		}
+	
+	}
+
+
+
+	
+
+	
+	
---
+++ tems.js
@@ -0,0 +1,40 @@
+//VARIAVEIS UTILIZADAS NO PROGRAMA
+var primeiro = document.querySelector("#primeiro");
+var nav = document.querySelector("nav");
+var body = document.querySelector("body");
+var rodape= document.querySelector("#rodape");
+var botao=document.querySelector(".botao");
+var select=document.querySelector("select");
+var boto=document.querySelector(".boto");
+
+//ALERTA 1 
+alert("  --------------------BEM VINDOS!------------------------ ");
+
+//MUDAR TEMA DA PÁGINA	                
+bot.onclick = function(){
+	alert("ATENÇÃO:Ao clicar na logo 'MUSICA' o tema da página será alterado e retornará ao tema original somente quando a página for atualizada ,OU,quando clicares em um dos títulos no cabeçalho antes do menu nav;:");
+	primeiro.style.backgroundColor = "#0d3b66";
+	nav.style.backgroundColor=" #f0544f";
+	document.body.style.backgroundImage = "url('musica1.png')";
+    rodape.style.backgroundColor="#0d3b66";
+    botao.style.backgroundColor="#0d3b66";
+    select.style.backgroundColor="#0d3b66";
+    
+
+};
+//VOLTAR AO TEMA ORIGINAL DA PÁGINA
+boto.onclick=function(){
+	primeiro.style.backgroundColor = "#008B8B";
+	nav.style.backgroundColor=" black";
+	document.body.style.backgroundImage = "url('music3.png')";
+    rodape.style.backgroundColor="#008B8B";
+    botao.style.backgroundColor="#008B8B";
+    select.style.backgroundColor="#008B8B";
+
+
+}
+botao.onclick = function(){
+		alert("ok");
+
+	}
+
